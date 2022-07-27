22494426234a8ba3437b52f5a491b69eec8a80c2
Pankaj Redekar <pankajredekar@outlook.com>
2022-07-27 17:22:34 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,13 +1,16 @@
 from django.template import Library
 from django.utils.html import format_html
 
+from blog.models import Post
+
 register = Library()
 
-@register.filter
-def author_details(author, current_user):
-    # if not isinstance(author, user_model):
-    #     # return empty string as safe default
-    #     return ""
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
 
     if author == current_user:
         return format_html("<strong>me</strong>")
@@ -18,11 +21,34 @@ def author_details(author, current_user):
         name = f"{author.username}"
 
     if author.email:
-        email = author.email
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
     else:
-        email = ""
+        prefix = ""
+        suffix = ""
 
-    html_text = format_html('<a href="mailto:{}">{}</a>', email, name) if email else name
+    return format_html("{}{}{}", prefix, name, suffix)
 
-    return html_text
 
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html(f'<div class="row {extra_classes}">')
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html(f'<div class="col {extra_classes}">')
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
